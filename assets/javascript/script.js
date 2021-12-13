9a0394f7b534df72df296c4fa2bf61bf13458e19
BITCHARD <richgarcia7525@gmail.com>
2021-12-12 21:58:03 -0600

    added html for city info and added the current weather statement along with the icons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,53 @@
-moment().format('L');
+moment().format("L");
 
 //Current City Weather//
 function searchCity(cityname) {
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityname +
+    "&units=imperial&appid=6fbc1c87443bfca2e8246c2411b29403";
+  var queryURLforcast =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityname +
+    "&units=imperial&appid= 6fbc1c87443bfca2e8246c2411b29403";
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=6fbc1c87443bfca2e8246c2411b29403";
-    var queryURLforcast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=6fbc1c87443bfca2e8246c2411b29403";
+  //create HTML for city information......
+  var cityNameEl = $("<h2>").text(response.name);
+  var displayMainDate = cityNameEl.append(" " + mainDate);
+  var tempEL = $("<p>").text("Tempraturer: " + response.main.temp);
+  var humEl = $("<p>").text("Humidity: " + response.main.humidity);
+  var windEl = $("<p>").text("Wind Speed: " + response.wind.speed);
+  var currentweather = response.weather[0].main;
 
-    
- 
-
-        //create HTML for city information......
-        var cityNameEl = $("<h2>").text(response.name);
-        var displayMainDate = cityNameEl.append(" " + mainDate);
-        var tempEL = $("<p>").text("Tempraturer: " + response.main.temp);
-        var humEl = $("<p>").text("Humidity: " + response.main.humidity);
-        var windEl = $("<p>").text("Wind Speed: " + response.wind.speed);
-        var currentweather = response.weather[0].main;
-
-        
+  if (currentweather === "Rain") {
+    var currentIcon = $("<img>").attr(
+      "src",
+      "http://openweathermap.org/img/wn/09d.png"
+    );
+    currentIcon.attr("style", "height: 60px; width: 60px");
+  } else if (currentweather === "Clouds") {
+    var currentIcon = $("<img>").attr(
+      "src",
+      "http://openweathermap.org/img/wn/03d.png"
+    );
+    currentIcon.attr("style", "height: 60px; width: 60px");
+  } else if (currentweather === "Clear") {
+    var currentIcon = $("<img>").attr(
+      "src",
+      "http://openweathermap.org/img/wn/01d.png"
+    );
+    currentIcon.attr("style", "height: 60px; width: 60px");
+  } else if (currentweather === "Drizzle") {
+    var currentIcon = $("<img>").attr(
+      "src",
+      "http://openweathermap.org/img/wn/10d.png"
+    );
+    currentIcon.attr("style", "height: 60px; width: 60px");
+  } else if (currentweather === "Snow") {
+    var currentIcon = $("<img>").attr(
+      "src",
+      "http://openweathermap.org/img/wn/13d.png"
+    );
+    currentIcon.attr("style", "height: 60px; width: 60px");
+  }
+  
---
+++ config.js
@@ -0,0 +1,3 @@
+var config = {
+  secret_api_key: "6fbc1c87443bfca2e8246c2411b29403",
+};
